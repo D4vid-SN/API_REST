e8edeae5f5319e233e4c34aecb05d781e68f9432
David Angarita Rodriguez <119277268+D4vid-SN@users.noreply.github.com>
2023-08-25 07:45:08 -0500

    Update modelos.py

--- API/flaskr/modelos/modelos.py
+++ API/flaskr/modelos/modelos.py
@@ -3,38 +3,41 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class cancion(db.Model):
-    id = db.Column(db.Integer, primay_key = True)
-    titulo = db.Column(db.string(128))
-    minutos = db.Columnc(db.Integer)
-    segundos = db.Columnc(db.Integer)
-    interprete = db.Columnc(db.String(128))
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(128))
+    minutos = db.Column(db.Integer)
+    segundos = db.Column(db.Integer)
+    interprete = db.Column(db.String(128))
 
     def __repr__(self):
-    return "{}-{}-{}-{}".format(*args: self.titulo, self.minutos, self.interprete)
+        return "{}-{}-{}-{}".format(self.titulo, self.minutos, self.segundos, self.interprete)
+
 
 class album(db.Model):
-    id = db.Column(db.Integer, primay_key = True)
-    titulo = db.Column(db.string(128))
-    año = db.Columnc(db.Integer)
-    descripion = db.Columnc(db.String(128))
-    medio = db.Columnc(db.Medio)
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(128))
+    year = db.Column(db.Integer)
+    descripion = db.Column(db.String(128))
+    medio = db.Column(db.String(128))
 
     def __repr__(self):
-    return "{}-{}-{}-{}".format(*args: self.titulo, self.año, self.segundos,self.interprete)
+        return "{}-{}-{}-{}".format(self.titulo, self.year, self.descripion, self.medio)
+
 
 class usuario(db.Model):
-    id = db.Column(db.Integer, primay_key = True)
-    nombre = db.Column(db.string(128))
-    contraseña = db.Columnc(db.String(128))
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(128))
+    password = db.Column(db.String(128))
 
     def __repr__(self):
-    return "{}-{}".format(*args: self.nombre, self.contraseña)
+        return "{}-{}".format(self.nombre, self.password)
 
-class Medio(db.Model):
-    id = db.Column(db.Integer, primay_key = True)
-    Disco = db.Column(db.string(128))
-    Casete = db.Columnc(db.String(128))
-    CD = db.Columnc(db.String(128))
+
+class medio(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    disco = db.Column(db.String(128))
+    casete = db.Column(db.String(128))
+    CD = db.Column(db.String(128))
 
     def __repr__(self):
-    return "{}-{}-{}".format(*args: self.Disco, self.Casete, self.CD)
+        return "{}-{}-{}".format(self.disco, self.casete, self.CD)
